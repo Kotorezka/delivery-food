b67180e910b2ed11b81f4e764661dbadd12cfe3c
Илья Богачев <Nate-Death@Yandex.ru>
2020-05-08 17:34:32 +0400

    day4

--- js/main.js
+++ js/main.js
@@ -15,12 +15,21 @@ restaurants = document.querySelector('.restaurants'),
 menu = document.querySelector('.menu'),
 logo = document.querySelector('.logo'),
 cardsMenu = document.querySelector('.cards-menu'),
+restaurantTitle = document.querySelector('.restaurant-title'),
+rating = document.querySelector('.rating'),
+minPrice = document.querySelector('.price'),
+category = document.querySelector('.category'),
+modalBody = document.querySelector('.modal-body'),
+modalPrice = document.querySelector('.modal-pricetag'),
+buttonClearCart = document.querySelector('.clear-cart'),
 buttonOut = document.querySelector('.button-out');
 
 //============/OF RECEIRT==============================
 
 //============VARIABLE==============================
 let login = localStorage.getItem('delivery');
+
+const cart = [];
 
 //============/VARIABLE=============================
 
@@ -47,6 +56,7 @@ const authorized = () => {
 		userName.style.display = '';
 		buttonOut.style.display = '';
 		buttonAuth.style.display = '';
+		cartButton.style.display = '';
 		buttonOut.removeEventListener('click', logOut);
 		localStorage.removeItem('delivery');
 		checkAuth();
@@ -56,7 +66,8 @@ const authorized = () => {
 	userName.textContent = login;
 	buttonAuth.style.display = 'none';
 	userName.style.display = 'inline';
-	buttonOut.style.display = 'block';
+	buttonOut.style.display = 'flex';
+	cartButton.style.display = 'flex';
 	buttonOut.addEventListener('click', logOut);
 };
 
@@ -90,7 +101,8 @@ const noAuthorized = () => {
 const createCardRestaurant = ({ image, kitchen, name, price, stars, products, time_of_delivery: timeOfDelivery, }) => {
 	
 	const card = `					
-		<a class="card card-restaurant" data-products="${products}">
+		<a class="card card-restaurant" data-products="${products}"
+		data-info="${[ name, price, stars, kitchen ]}">
 			<img src="${image}" alt="image" class="card-image"/>
 			<div class="card-text">
 				<div class="card-heading">
@@ -115,6 +127,7 @@ const createCardGood = ({ description, name, id, image,	price }) => {
 
 	const card = document.createElement('div');
 	card.className = 'card';
+	
 	card.insertAdjacentHTML('beforeend',`
 			<img src="${image}" alt="image" class="card-image"/>
 			<div class="card-text">
@@ -126,14 +139,15 @@ const createCardGood = ({ description, name, id, image,	price }) => {
 					</div>
 				</div>
 				<div class="card-buttons">
-					<button class="button button-primary button-add-cart">
+					<button class="button button-primary button-add-cart" id=${id}>
 						<span class="button-card-text">В корзину</span>
 						<span class="button-cart-svg"></span>
 					</button>
-					<strong class="card-price-bold">${price} ₽</strong>
+					<strong class="card-price card-price-bold">${price} ₽</strong>
 				</div>
 			</div>
 	`);
+	console.log(id);
 	cardsMenu.insertAdjacentElement('beforeend',card);
 }
 
@@ -144,10 +158,19 @@ const openGoods = (event) => {
 	if(login) {
 		console.log('Все окей');
 		if (restaurant) {
+			const info = restaurant.dataset.info.split(',');
+			const [ name, price, stars, kitchen ] = info;
 			cardsMenu.textContent = '';
 			containerPromo.classList.add('hide');
 			restaurants.classList.add('hide');
 			menu.classList.remove('hide');
+			
+			restaurantTitle.textContent = name;
+			rating.textContent = stars;
+			minPrice.textContent = `От ${price} ₽`;
+			category.textContent = kitchen;
+			
+			
 			getData(`./db/${restaurant.dataset.products}`).then(function(data){
 				data.forEach(createCardGood);
 			})
@@ -165,14 +188,90 @@ const returnMain = () => {
 
 }
 
+const addToCart = (event) => {
+	const target = event.target;
+	const buttonAddToCart = target.closest('.button-add-cart');
+	if (buttonAddToCart) {
+		const card = target.closest('.card');
+		const title = card.querySelector('.card-title-reg').textContent;
+		const cost = card.querySelector('.card-price').textContent;
+		const id = buttonAddToCart.id;
+		console.log(id);
+		const food = cart.find(function(item){
+			return item.id === id;
+		})
+
+		
+		
+		if (food) {
+			food.count += 1;
+		} else {
+			cart.push({
+				id,
+				title,
+				cost,
+				count: 1
+				})
+		};
+	}
+	
+}
+
+const renderCart = () => {
+	modalBody.textContent = '';
+	cart.forEach(function({ id, title, cost, count }){
+		const itemCart = `
+			<div class="food-row">
+				<span class="food-name">${title}</span>
+				<strong class="food-price">${cost}</strong>
+				<div class="food-counter">
+					<button class="counter-button counter-minus" data-id=${id}>-</button>
+					<span class="counter">${count}</span>
+					<button class="counter-button counter-plus" data-id=${id}>+</button>
+				</div>
+			</div>
+		`;
+		modalBody.insertAdjacentHTML('afterbegin', itemCart);
+	})
+	const totalPrice = cart.reduce(function(result, item){
+		return result + (parseFloat(item.cost) * item.count);	
+	}, 0);
+
+	modalPrice.textContent = totalPrice + ' ₽';
+}
+
+const changeCount = (event) => {
+	const target = event.target;
+	if (target.classList.contains('counter-button')) {
+		const food = cart.find(function (item) {return item.id === target.dataset.id; });
+		if (target.classList.contains('counter-minus')){
+			food.count--;
+			if (food.count === 0 ) {
+				cart.splice(cart.indexOf(food),1);
+			}
+		}
+		if (target.classList.contains('counter-plus')){
+			food.count++;
+		}
+		renderCart();
+	}
+}
+
 function init () {
 	getData('./db/partners.json').then(function (data) {
 		data.forEach(createCardRestaurant);
 	});
 
-
-
-	cartButton.addEventListener("click", toggleModal);
+	buttonClearCart.addEventListener('click', function(){
+		cart.length = 0;
+		renderCart();
+	})	
+	modalBody.addEventListener('click', changeCount);
+	cardsMenu.addEventListener('click', addToCart);
+	cartButton.addEventListener("click", function(){
+		renderCart();
+		toggleModal();
+	});
 	close.addEventListener("click", toggleModal);
 	cardsRestaurants.addEventListener('click', openGoods);
 	logo.addEventListener('click', returnMain);
